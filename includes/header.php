8776280612448f1933c03a72020e86b26ab5457d
MigFOliv <up202208340@g.uporto.pt>
2025-04-18 19:15:48 +0100

    cabecalho

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Freelance Platform</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <h1>Freelance Platform</h1>
+    <nav>
+        <a href="index.php">Início</a> |
+        <a href="services.php">Serviços</a> |
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="dashboard.php">Painel</a> |
+            <a href="logout.php">Logout (<?= htmlspecialchars($_SESSION['username']) ?>)</a>
+        <?php else: ?>
+            <a href="login.php">Login</a> |
+            <a href="register.php">Registar</a>
+        <?php endif; ?>
+    </nav>
+    <hr>
+</header>
