e566121d67634a67abb83014f56c99b7751ea4fb
MigFOliv <up202208340@g.uporto.pt>
2025-05-02 09:53:00 +0100

    login atualizado

--- login.php
+++ login.php
@@ -15,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($user && password_verify($password, $user['password_hash'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
+        $_SESSION['is_admin'] = $user['is_admin'];
         header("Location: pages/dashboard.php");
         exit();
     } else {
